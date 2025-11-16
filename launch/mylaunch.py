08fa27c99a47c7233e55bde63cc524fc29310352
s3gf4u17 <szymon.kolakowski@gmail.com>
2025-11-17 00:23:45 +0100

    test launch with python server

--- launch/mylaunch.py
+++ launch/mylaunch.py
@@ -1,11 +1,22 @@
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess, RegisterEventHandler
-from launch.event_handlers import OnShutdown
+from launch.event_handlers import OnShutdown, OnProcessExit
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
+    pkg_share=get_package_share_directory("ros2_lidar_georeference")
+    setup_script=os.path.join(pkg_share,"script","setup.sh")
+
     setup = ExecuteProcess(
-        cmd=["ls"],
+        cmd=["bash",setup_script],
+        shell=False,
+        output="screen",
+    )
+
+    web_server = ExecuteProcess(
+        cmd=["python3","-m","http.server","9000"],
         shell=False,
         output="screen",
     )
@@ -23,8 +34,15 @@ def generate_launch_description():
         output="screen",
     )
 
+    after_setup = RegisterEventHandler(
+        OnProcessExit(
+            target_action=setup,
+            on_exit=[hello_world_node]
+        )
+    )
+
     on_shutdown = RegisterEventHandler(
         OnShutdown(on_shutdown=[cleanup])
     )
 
-    return LaunchDescription([setup,hello_world_node,on_shutdown])
+    return LaunchDescription([setup,after_setup,on_shutdown])
