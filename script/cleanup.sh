08b64b7cb2dd3dea1620f1187acafed97beeb389
s3gf4u17 <szymon.kolakowski@gmail.com>
2025-12-07 05:53:43 +0100

    move nginx injection to launch scripts

--- script/cleanup.sh
+++
@@ -1,7 +0,0 @@
-unset ROS2_LIDAR_GEOREFERENCE_DATA
-
-dest=/opt/leo_ui
-
-sudo rm "$dest/ros2_lidar_georeference.html"
-sudo rm "$dest/css/ros2_lidar_georeference.css"
-sudo rm "$dest/js/ros2_lidar_georeference.js"
--- script/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# inject web files into existing nginx setup
-pkg_share=$(ros2 pkg prefix --share ros2_lidar_georeference)
-dest=/opt/leo_ui
-
-sudo cp "$pkg_share/web/ros2_lidar_georeference.html" "$dest/"
-sudo cp "$pkg_share/web/css/ros2_lidar_georeference.css" "$dest/css/"
-sudo cp "$pkg_share/web/js/ros2_lidar_georeference.js" "$dest/js/"
